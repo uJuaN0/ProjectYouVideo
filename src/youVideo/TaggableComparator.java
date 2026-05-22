c17cb98afcb51474c477ce429309cba00079b8a9
Juan Lima <nauj686@gmail.com>
2026-05-22 22:57:44 +0100

    ADD -  comparators to help with tagged

---
+++ src/youVideo/TaggableComparator.java
@@ -0,0 +1,12 @@
+package youVideo;
+
+import java.util.Comparator;
+
+public class TaggableComparator implements Comparator<Taggable> {
+    @Override
+    public int compare(Taggable t1, Taggable t2) {
+        int byTitle = t1.getTitle().compareToIgnoreCase(t2.getTitle());
+        if (byTitle != 0) return byTitle;
+        return t1.getTagOrder() - t2.getTagOrder();
+    }
+}
---
+++ src/youVideo/TaggableDescComparator.java
@@ -0,0 +1,12 @@
+package youVideo;
+
+import java.util.Comparator;
+
+public class TaggableDescComparator implements Comparator<Taggable> {
+    @Override
+    public int compare(Taggable t1, Taggable t2) {
+        int byTitle = t2.getTitle().compareToIgnoreCase(t1.getTitle());
+        if (byTitle != 0) return byTitle;
+        return t1.getTagOrder() - t2.getTagOrder();
+    }
+}
