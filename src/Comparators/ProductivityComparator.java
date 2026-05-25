95e4eb0f816e678bdf72be18884a0e586787db87
Juan Lima <nauj686@gmail.com>
2026-05-25 15:07:53 +0100

    change - comparators, change on logic

--- src/Comparators/ProductivityComparator.java
+++ src/Comparators/ProductivityComparator.java
@@ -8,10 +8,12 @@ public class ProductivityComparator implements Comparator<Author> {
 
     @Override
     public int compare(Author o1, Author o2) {
-        if (o1.getProductivity() > o2.getProductivity())
+        if (o1.getProductivity() > o2.getProductivity()){
             return -1;
-        if (o1.getProductivity() < o2.getProductivity())
+        }
+        if (o1.getProductivity() < o2.getProductivity()){
             return 1;
+        }
         return o1.getName().compareToIgnoreCase(o2.getName());
     }
 
--- src/Comparators/TaggableDescComparator.java
+++ src/Comparators/TaggableDescComparator.java
@@ -8,7 +8,9 @@ public class TaggableDescComparator implements Comparator<Taggable> {
     @Override
     public int compare(Taggable t1, Taggable t2) {
         int byTitle = t2.getTitle().compareToIgnoreCase(t1.getTitle());
-        if (byTitle != 0) return byTitle;
+        if (byTitle != 0){
+            return byTitle;
+        }
         return t1.getTagOrder() - t2.getTagOrder();
     }
 }
